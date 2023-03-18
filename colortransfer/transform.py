ba395b9b0362d8dd3aba399b093b0bd22de34713
Sreerag M <sreeragibtl@gmail.com>
2023-03-18 19:07:35 +0530

    dev: Implement transform

--- colortransfer/transform.py
+++ colortransfer/transform.py
@@ -14,4 +14,30 @@ from numpy.typing import NDArray
 
 # fmt: on
 
-transform: Callable[[NDArray, NDArray], NDArray]
+
+def transform(
+    source: NDArray,
+    target: NDArray,
+    std: Callable[[NDArray], float],
+    mean: Callable[[NDArray], float],
+    subtract: Callable[[NDArray, NDArray], NDArray],
+    multiply: Callable[[NDArray, float], NDArray],
+) -> NDArray:
+    """Transform arrays.
+
+    Transform arrays using Reinhard method.
+
+    Arguments:
+        source: Source lab channel.
+        target: Target lab channel.
+
+    Returns:
+        Transformed channel array.
+
+    """
+    std_source = std(source)
+    std_target = std(target)
+    mean_source = mean(source)
+    difference = subtract(source, mean_source)
+    ratio = std_target.__truediv__(std_source)
+    return multiply(ratio, difference)
