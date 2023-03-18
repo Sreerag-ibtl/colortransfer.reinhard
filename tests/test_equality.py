3b79c365cdf890444e12d23daccea7681eb80b6e
Sreerag M <sreeragibtl@gmail.com>
2023-03-18 14:53:31 +0530

    test: Add function test_equality

--- tests/test_equality.py
+++ tests/test_equality.py
@@ -6,3 +6,30 @@ Usage:
     python -m pytest -m -n auto tests.test_equality
 
 """
+
+# fmt: off
+from typing import Any, Callable
+
+# fmt: on
+
+
+def test_equality(
+    lhs: Any, rhs: Any, assert_equality: Callable[[Any, Any], None]
+) -> None:
+    """Test equality.
+
+    Test equality of lhs and rhs using assert_equality.
+
+    Arguments:
+        lhs: Left hand side.
+        rhs: Right hand side.
+        assert_equality: Function asserting equality.
+
+    Raises:
+        AssertionError
+
+    Returns:
+        None
+
+    """
+    assert_equality(lhs, rhs)
